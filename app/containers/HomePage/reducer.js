4f771fbc52bdfb8485576a5e9ac6f13845680ddc
Jordis Man <jordisman@gmail.com>
2019-10-09 12:06:14 -0700

    Added actions and reducer files in HomePage

---
+++ app/containers/HomePage/reducer.js
@@ -0,0 +1,21 @@
+import {ADD_NOTE} from './actions';
+
+const initialState = {
+  notes: []
+}
+
+const homeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_NOTE:
+      const newNote = {
+        note: action.noteData.note
+      }
+      return {
+        ...state,
+        notes: state.notes.concat(newNote)
+      }
+  }
+  return state;
+};
+
+export default homeReducer;
